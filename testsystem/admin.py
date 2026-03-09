74e5fd4d10b4f95a5bee5412b6d8bc6313cbb7f9
beznosov_design <beznosov@gmail.com>
2026-03-10 00:18:14 +0300

    initial_commit вывод времени вывод времени1

--- testsystem/admin.py
+++ testsystem/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Test, Question, Answer, Result
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['test', 'text', 'image', 'allow_text_answer']
+    # Это позволит писать формулы в админке с подсветкой синтаксиса
+
 admin.site.register(Test)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
 admin.site.register(Result)
